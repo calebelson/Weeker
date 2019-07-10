19f81e3fd5e06cb3617c8a10419a5250e5515a98
calebelson <caleb.elson@gmail.com>
2019-07-10 17:02:15 -0500

    AcknowledgmentsVC now links to Ted Talk youtube video

--- Weeker/Views/DOB SubViews/AcknowledgmentsViewController.swift
+++ Weeker/Views/DOB SubViews/AcknowledgmentsViewController.swift
@@ -7,7 +7,33 @@
 //
 
 import UIKit
+import SafariServices
 
 class AcknowledgmentsViewController: UIViewController {
+    @IBOutlet weak var tedTalkLabel: UILabel!
+    let theme = ThemeManager.currentTheme()
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tedTalkLabel.textColor = theme.primaryColor
+        
+        let inspirationText = "App inspired by"
+        let youtubeLink = "this Ted Talk on procrastination"
+
+        let labelString = "\(inspirationText)\n\(youtubeLink)"
+
+        let range = (labelString as NSString).range(of: youtubeLink)
+        let attributedString = NSMutableAttributedString.init(string: labelString)
+        attributedString.addAttribute(NSAttributedString.Key.foregroundColor, value: theme.secondaryColor, range: range)
+
+        tedTalkLabel.attributedText = attributedString
+    }
+
+    
+    @IBAction func tedTalkLabelTapped(_ sender: Any) {
+        let myURL = URL(string: "https://www.youtube.com/watch?v=arj7oStGLkU")
+        let tedTalkVideoVC = SFSafariViewController(url: myURL!)
+        present(tedTalkVideoVC, animated: true)
+    }
 }
